0847bbc6895235d2786179fc657ed5db7f8194fd
longDo0Dev <dongenjoy26@gmail.com>
2025-06-14 11:22:09 +0700

    เพิ่มไฟล์เว็บไซต์เริ่มต้น

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>เว็บไซต์แรกของฉัน</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>สวัสดี โลก!</h1>
+    <p>นี่คือเว็บไซต์แรกของฉันที่ใช้ PHP</p>
+</body>
+</html>
